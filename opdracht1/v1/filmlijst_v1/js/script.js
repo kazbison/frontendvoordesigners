914da588f4be8ac7116383d70e36ae29d644a55d
Kaz Bison <ksbison@gmail.com>
2019-04-24 11:45:26 +0200

    versie 1 af
    De hartjes zijn nu klikbaar d.m.v. de javascript

--- opdracht1/v1/filmlijst_v1/js/script.js
+++ opdracht1/v1/filmlijst_v1/js/script.js
@@ -2,41 +2,39 @@
 /*eslint-env browser*/
 /*eslint 'no-console': 0*/
 
-var hartjes = document.querySelectorAll(".hartje");
+var hartje = document.querySelectorAll(".hartje");
+var aantalHartjes = hartje.length;
+console.log(aantalHartjes);
+
 
 var favorieten = document.querySelector("#aantal_favorieten");
+//var aantalFavorieten = document.querySelector(".favoriet").length;
 var aantalFavorieten = 0;
 
 
 
 
+function klik() {
+
+    //console.log(this.src);
+
+    if (!this.src.includes("niet_toegevoegd.svg")) {
+        console.log('verwijderd');
+        this.src = '../images/niet_toegevoegd.svg';
+        //        hartje.removeClass("favoriet");
+
+    } else {
+        console.log('toegevoegd');
+        this.src = '../images/toegevoegd.svg';
+        //        hartje.addClass("favoriet");
+
+    }
+}
+
+
+
+for (var i = 0; i < aantalHartjes; i++) {
+    hartje[i].addEventListener('click', klik, false);
+}
+
 favorieten.textContent = ' (' + aantalFavorieten + ')';
-
-
-
-//hartjes.src = "./images/toegevoegde_film.svg";
-
-console.log(hartjes.length);
-
-//hartjes.classList.add("toegevoegd_aan_favorieten");
-
-
-//function toevoegenOfVerwijderen() {
-//
-//
-//    if (hartjes.classList.contains("toegevoegd_aan_favorieten")) {
-//
-//        hartjes.classList.remove("toegevoegd_aan_favorieten");
-//
-//    } else {
-//        hartjes.classList.add("toegevoegd_aan_favorieten");
-//    }
-//
-//}
-//
-//hartjes.addEventListener('click', toevoegenOfVerwijderen, false);
-
-hartjes.addEventListener('click', function () {
-    hartjes.src = "../images/toegevoegde_film.svg";
-    //    hartjes.classList.add("toegevoegd_aan_favorieten");
-}, false);

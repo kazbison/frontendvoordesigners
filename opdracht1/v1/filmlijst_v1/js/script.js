07760af7a97719ecf137568535f74c2ccdfe9dd0
Kaz Bison <ksbison@gmail.com>
2019-04-24 09:43:27 +0200

    css (gird) gegeven
    Wat elementen gestijld met CSS en een grid toegevoegd d.m.v. CSS

--- opdracht1/v1/filmlijst_v1/js/script.js
+++ opdracht1/v1/filmlijst_v1/js/script.js
@@ -2,3 +2,41 @@
 /*eslint-env browser*/
 /*eslint 'no-console': 0*/
 
+var hartjes = document.querySelectorAll(".hartje");
+
+var favorieten = document.querySelector("#aantal_favorieten");
+var aantalFavorieten = 0;
+
+
+
+
+favorieten.textContent = ' (' + aantalFavorieten + ')';
+
+
+
+//hartjes.src = "./images/toegevoegde_film.svg";
+
+console.log(hartjes.length);
+
+//hartjes.classList.add("toegevoegd_aan_favorieten");
+
+
+//function toevoegenOfVerwijderen() {
+//
+//
+//    if (hartjes.classList.contains("toegevoegd_aan_favorieten")) {
+//
+//        hartjes.classList.remove("toegevoegd_aan_favorieten");
+//
+//    } else {
+//        hartjes.classList.add("toegevoegd_aan_favorieten");
+//    }
+//
+//}
+//
+//hartjes.addEventListener('click', toevoegenOfVerwijderen, false);
+
+hartjes.addEventListener('click', function () {
+    hartjes.src = "../images/toegevoegde_film.svg";
+    //    hartjes.classList.add("toegevoegd_aan_favorieten");
+}, false);

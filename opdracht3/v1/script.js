4f82d6c38944a602de63e2ba556007941a8525c3
Kaz Bison <ksbison@gmail.com>
2019-06-05 09:31:52 +0200

    Update script.js

--- opdracht3/v1/script.js
+++ opdracht3/v1/script.js
@@ -1,8 +1,7 @@
-var uri = "https://koopreynders.github.io/frontendvoordesigners/opdracht3/json/movies.json";
+/*var uri = "https://koopreynders.github.io/frontendvoordesigners/opdracht3/json/movies.json";
 
 var section = document.querySelector('section');
-console.log("section",section);
-
+console.log("section", section);
 
 console.log("loadimagesmetXHR");
 var request = new XMLHttpRequest();
@@ -10,10 +9,45 @@ request.open('get', uri);
 request.responseType = 'json';
 request.send();
 
-request.addEventListener("load", function(){
-  var data = request.response;
-  console.log("request is geladen: ",request.response);
-  //er is data
-  //nu kun je iets doen
-  section.textContent = JSON.stringify(data);
+request.addEventListener("load", function () {
+    var data = request.response;
+    console.log("request is geladen: ", request.response);
+
+
+    //er is data
+    //nu kun je iets doen
+    section.textContent = JSON.stringify(data);
+
+});*/
+
+
+
+var uri = "https://koopreynders.github.io/frontendvoordesigners/opdracht3/json/movies.json";
+
+var section = document.querySelector('section');
+console.log("section", section);
+
+console.log("loadimagesmetXHR");
+var request = new XMLHttpRequest();
+request.open('get', uri);
+request.responseType = 'json';
+request.send();
+
+request.addEventListener("load", function () {
+    var data = request.response;
+    console.log("request is geladen: ", request.response);
+
+
+    //er is data
+    //nu kun je iets doen
+    section.textContent = JSON.stringify(data);
+
 });
+
+
+
+
+
+
+//data.genres
+//data[0]['genres'][0]

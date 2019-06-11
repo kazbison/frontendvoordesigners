09a778c25139b5ad7531eb48fd9da1df19f3983f
Kaz Bison <ksbison@gmail.com>
2019-06-11 22:41:16 +0200

    Opdracht 3 v1
    met mousover verdwijnt image en komt video tevoorschijn en begint met afspelen

--- opdracht3/v1/script.js
+++ opdracht3/v1/script.js
@@ -22,17 +22,39 @@ function jsonInHtml(myJson) {
         document.querySelector('main section').appendChild(createArticle);
 
 
-        //creëert plaatje + de source in de article
+        /*   Alle content in overzicht   */
+
+        //creëert video tag + de source in de video tag
+        var createVideo = document.createElement('video');
+        createArticle.appendChild(createVideo);
+        createVideo.src = myJson[i].trailer;
+        createVideo.setAttribute("controls", "controls");
+        createVideo.onmouseover = function () {
+            this.play();
+        };
+        // als je je muis van een afbeelding af haalt komt de afbeelding terug
+        createVideo.onmouseout = function () {
+            this.pause();
+        };
+
+
+        //creëert img tag + de source in de img tag
         var createImage = document.createElement('img');
         createArticle.appendChild(createImage);
         createImage.src = myJson[i].cover;
-
+        // als je je muis boven een afbeeldingen houd verdwijnt de afbeelding
+        createImage.onmouseover = function () {
+            this.style.visibility = 'hidden';
+        };
+        // als je je muis van een afbeelding af haalt komt de afbeelding terug
+        createImage.onmouseout = function () {
+            this.style.visibility = 'visible';
+        };
 
         //creëert titel in de article
         var createTitel = document.createElement('h2');
         createArticle.appendChild(createTitel);
         createTitel.innerHTML = myJson[i].title;
-
 
         //creëert footer in de article
         var createFooter = document.createElement('footer');
@@ -49,6 +71,36 @@ function jsonInHtml(myJson) {
         createP2.innerHTML = myJson[i].reviews[i].score;
 
 
-    }
+        /*   Alle content die getoond wordt als op de article wordt geklikt en het hierdoor de class "show_details" heeft gekregen   */
 
+
+    }
 }
+
+//function isDramaFilm(value) {
+//
+//
+//    return value === "drama";
+//
+//
+//}
+
+//button.addEventListener("click", function (value) {
+//
+//    var filtered = myJson.filter(isDramaFilm);
+//
+//}, false);
+
+
+
+
+//article.addEventListener("mouseover", function () {
+//
+//    /*   Bron: https://codeburst.io/learn-understand-javascripts-filter-function-bde87bce206   */
+//    let cities = data.filter(val => {
+//        return val.population > 500000000;
+//    });
+//
+//
+//
+//}, false);
